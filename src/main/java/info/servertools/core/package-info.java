6f3eb73e01f0366db26797a29d456499e83230b7
Matthew Prenger <matthewprenger@gmail.com>
2016-01-01 21:01:05 -0600

    Fix license headers

--- src/main/java/info/servertools/core/package-info.java
+++ src/main/java/info/servertools/core/package-info.java
@@ -1,8 +1,8 @@
 /*
  * This file is a part of ServerTools <http://servertools.info>
  *
- * Copyright (c) 2015 ServerTools
- * Copyright (c) 2015 contributors
+ * Copyright (c) 2014 ServerTools
+ * Copyright (c) 2014 contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
